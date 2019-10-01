f5223d0570655e75abecb049edefe450cf0333ec
aobrazovskaya <anna.obrazovskaya@gmail.com>
2019-10-01 22:46:49 +0300

    исправлены ошибки проверки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var adsNumbers = 8;
 var mapElement = document.querySelector('.map');
 var PIN_WIDTH = 50;
@@ -6,8 +8,8 @@ var mapWidth = mapElement.offsetWidth - PIN_WIDTH;
 var MAP_BEGIN_HEIGHT = 130 - PIN_HEIGHT;
 var MAP_END_HEIGHT = 630 - PIN_HEIGHT;
 var titleArray = ['Милая квартирка недалеко от метро', 'Квартира недорого', 'Квартира в Токио'];
-var flatTypes = ['palace','flat','house','bungalo'];
-var checkins = ['12:00','13:00','14:00'];
+var flatTypes = ['palace', 'flat', 'house', 'bungalo'];
+var checkins = ['12:00', '13:00', '14:00'];
 var featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var maxNumbderOfRooms = 10;
@@ -19,7 +21,7 @@ var maxNumberOfGuests = 20;
  */
 var createAds = function () {
   var ads = [];
-  
+
   for (var i = 0; i < adsNumbers; i++) {
     var avatar = 'img/avatars/user0' + (i + 1) + '.png';
     var title = titleArray[getRandomNumberInTheRange(0, titleArray.length)];
@@ -33,7 +35,7 @@ var createAds = function () {
       author: {
         avatar: avatar,
       },
-      
+
       offer: {
         title: title,
         address: address,
@@ -47,13 +49,13 @@ var createAds = function () {
         description: title + ' по адресу: ' + address,
         photos: getRandomQuantity(photos),
       },
-      
+
       location: {
         x: location.x,
         y: location.y,
       }
-    }
-  ads.push(ad);
+    };
+    ads.push(ad);
   }
   return ads;
 };
@@ -67,7 +69,7 @@ function getRandomNumberInTheRange(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 /**
  * Get random quantity of array elements.
@@ -83,16 +85,15 @@ function getRandomQuantity(array) {
     }
   }
   return arr;
-};
+}
 
 /**
- * Add DOM elements to HTML doc. 
+ * Add DOM elements to HTML doc.
  * @param {array} arr array of js elements
  * @return {object} fragment of DOM elements
  */
 function createDOMElements(arr) {
   var fragment = document.createDocumentFragment();
-  
   for (var i = 0; i < arr.length; i++) {
     fragment.appendChild(createPinElement(arr[i]));
   }
@@ -110,7 +111,6 @@ function createPinElement(user) {
   element.style = 'left: ' + (user.location.x + PIN_WIDTH / 2) + 'px; top: ' + (user.location.y + PIN_HEIGHT) + 'px;';
   element.children[0].src = user.author.avatar;
   element.children[0].alt = user.offer.title;
-  
   return element;
 }
 
