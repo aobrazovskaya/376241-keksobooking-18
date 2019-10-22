f39f91d0cec7e2690e01b9a294bc4bfc9bde8e2c
aobrazovskaya <anna.obrazovskaya@gmail.com>
2019-10-22 21:43:08 +0300

    Добавлены файлы для разделения main.js на модули

--- js/main.js
+++ js/main.js
@@ -433,15 +433,15 @@ function setPriceRequirements() {
   }
 }
 
-var formTimeIn = formElement.querySelector('#timein');
-var formTimeOut = formElement.querySelector('#timeout');
-formTimeIn.addEventListener('change', synchronizeTime);
-formTimeOut.addEventListener('change', synchronizeTime);
+// var formTimeIn = formElement.querySelector('#timein');
+// var formTimeOut = formElement.querySelector('#timeout');
+// formTimeIn.addEventListener('change', synchronizeTime);
+// formTimeOut.addEventListener('change', synchronizeTime);
 
-function synchronizeTime(evt) {
-  if (evt.target === formTimeIn) {
-    formTimeOut.value = formTimeIn.value;
-  } else {
-    formTimeIn.value = formTimeOut.value;
-  }
-}
+// function synchronizeTime(evt) {
+//   if (evt.target === formTimeIn) {
+//     formTimeOut.value = formTimeIn.value;
+//   } else {
+//     formTimeIn.value = formTimeOut.value;
+//   }
+// }
