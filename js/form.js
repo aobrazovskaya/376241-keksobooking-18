7ae9607c16287955f99cc59f6b7235b26b8ce708
aobrazovskaya <anna.obrazovskaya@gmail.com>
2019-11-15 15:45:35 +0300

    При загрузке страницы в поле адреса записываются координаты цента метки

--- js/form.js
+++ js/form.js
@@ -158,11 +158,22 @@
     * @param {HTMLElement} pin template clone of pin
     */
   function setPinAddress(pin) {
-    var address = (pin.offsetLeft + window.keksobooking.pin.MAIN_PIN_WIDTH / 2) + ', ' + (pin.offsetTop + window.keksobooking.pin.MAIN_PIN_HEIGHT);
+    var address = (pin.offsetLeft + window.keksobooking.pin.MAIN_PIN_WIDTH / 2) + ', '
+    + (pin.offsetTop + window.keksobooking.pin.MAIN_PIN_HEIGHT);
+    formAddress.value = address;
+  }
+
+  /**
+    * @param {HTMLElement} pin template clone of pin
+    */
+  function setInitPinAddress() {
+    var address = (window.keksobooking.pin.main.offsetLeft + window.keksobooking.pin.MAIN_PIN_WIDTH / 2) + ', '
+    + (window.keksobooking.pin.main.offsetTop + window.keksobooking.pin.MAIN_PIN_WIDTH / 2);
     formAddress.value = address;
   }
 
   function runModule() {
+    setInitPinAddress();
     setFormInitialPropertiesAndEvents();
     makeSuccessPopup();
     makeErrorPopup();
